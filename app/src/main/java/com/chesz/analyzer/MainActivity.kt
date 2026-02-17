0920d9e858b2081d17fe0c315ed1fd57b01245e0
ortiz202-a11y <ortiz202@gmail.com>
2026-02-17 16:28:01 -0600

    feat: icono 80dp, cierre con X y tema transparente

--- app/src/main/java/com/chesz/analyzer/MainActivity.kt
+++ app/src/main/java/com/chesz/analyzer/MainActivity.kt
@@ -4,71 +4,26 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.provider.Settings
-import android.widget.Toast
 import com.chesz.analyzer.bubble.BubbleService
 
 class MainActivity : Activity() {
-    private var openedSettings = false
-    private val h = Handler(Looper.getMainLooper())
-    private var tries = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Settings.canDrawOverlays(this)) {
+            startService(Intent(this, BubbleService::class.java))
+            finish()
+        } else {
+            val i = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            startActivity(i)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        h.removeCallbacksAndMessages(null)
-
         if (Settings.canDrawOverlays(this)) {
             startService(Intent(this, BubbleService::class.java))
-            h.postDelayed({ finishAndRemoveTask() }, 250)
-            return
+            finish()
         }
-
-        if (!openedSettings) {
-            openedSettings = true
-            Toast
-                .makeText(
-                    this,
-                    "Da permisos a chesz para 'Mostrar sobre otras apps'. Actívalo y regresa.",
-                    Toast.LENGTH_LONG,
-                ).show()
-
-            val i =
-                Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName"),
-                )
-            startActivity(i)
-            return
-        }
-
-        // Workaround: algunos Android tardan en reflejar el permiso al volver
-        tries = 0
-        h.postDelayed(::pollPermissionThenStart, 250)
-    }
-
-    private fun pollPermissionThenStart() {
-        if (Settings.canDrawOverlays(this)) {
-            startService(Intent(this, BubbleService::class.java))
-            h.postDelayed({ finishAndRemoveTask() }, 250)
-            return
-        }
-
-        tries++
-        if (tries >= 12) {
-            finishAndRemoveTask()
-            return
-        }
-        h.postDelayed(::pollPermissionThenStart, 250)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        h.removeCallbacksAndMessages(null)
     }
 }
