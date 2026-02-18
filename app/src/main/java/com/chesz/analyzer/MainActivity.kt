2e2b303b32107012f13d16e50c7163ce621fb3e3
ortiz202-a11y <ortiz202@gmail.com>
2026-02-18 03:11:34 -0600

    Auto-fix: Sintaxis validada estructuralmente

--- app/src/main/java/com/chesz/analyzer/MainActivity.kt
+++ app/src/main/java/com/chesz/analyzer/MainActivity.kt
@@ -2,12 +2,11 @@ package com.chesz.analyzer
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.chesz.analyzer.floating.FloatingService
+import com.chesz.analyzer.floating.BubbleService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
             text = "ARRANCAR BURBUJA"
             setOnClickListener {
                 if (checkPermission()) {
-                    startService(Intent(this@MainActivity, FloatingService::class.java))
+                    startService(Intent(this@MainActivity, BubbleService::class.java))
                 }
             }
         }
