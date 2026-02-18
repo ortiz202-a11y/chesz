b01a3665fa35578e84ae8e44e0fcbbcc6717c90c
ortiz202-a11y <ortiz202@gmail.com>
2026-02-18 03:11:34 -0600

    Auto-fix: Sintaxis validada estructuralmente

--- app/src/main/java/com/chesz/analyzer/floating/FloatingListener.kt
+++ app/src/main/java/com/chesz/analyzer/floating/FloatingListener.kt
@@ -2,5 +2,5 @@ package com.chesz.analyzer.floating
 
 interface FloatingListener {
     fun onFinishFloatingView()
-    fun伴随着(x: Int, y: Int) // Para rastrear posición si fuera necesario
+    fun onTouchMove(x: Int, y: Int)
 }
