9d1a3688318b0926a94566f3d29647296793873c
ortiz202-a11y <ortiz202@gmail.com>
2026-03-03 05:55:32 -0600

    Build: 2026-03-03 05:55:32 - Sincronización en master

---
+++ app/src/main/java/com/chesz/CapturePermissionActivity.kt
@@ -0,0 +1,34 @@
+package com.chesz
+
+import android.app.Activity
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Bundle
+import com.chesz.floating.BubbleService
+
+class CapturePermissionActivity : Activity() {
+
+  companion object {
+    const val REQ = 7001
+    const val ACTION_RESULT = "CHESZ_CAPTURE_PERMISSION_RESULT"
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val mgr = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    startActivityForResult(mgr.createScreenCaptureIntent(), REQ)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    val i = Intent(this, BubbleService::class.java).apply {
+      action = ACTION_RESULT
+      putExtra("resultCode", resultCode)
+      putExtra("data", data)
+    }
+    startService(i)
+
+    finish()
+  }
+}
