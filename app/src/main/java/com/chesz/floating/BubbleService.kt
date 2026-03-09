d4523afb3f6090b4fe44b4897aa539cb41fe8d95
ortiz202-a11y <ortiz202@gmail.com>
2026-03-09 17:03:53 -0600

    Fix: bton Premisos centro

--- app/src/main/java/com/chesz/floating/BubbleService.kt
+++ app/src/main/java/com/chesz/floating/BubbleService.kt
@@ -279,7 +279,7 @@ class BubbleService : Service() {
         val panelH = (dm.heightPixels * 0.20f).toInt()
 
         val rootX = rootLp.x
-        val rootY = rootLp.y - (panelH / 2 - dp(60) / 2)
+        val rootY = rootLp.y - (panelH - btnH)
         val rootW = panelW + (btnW / 2)
         val rootH = panelH
         val (sw, sh) = this.sw to this.sh
@@ -316,7 +316,7 @@ class BubbleService : Service() {
         bubbleWrap.layoutParams =
             FrameLayout.LayoutParams(btnW, btnH).apply {
                 leftMargin = 0
-                topMargin = (panelH / 2 - dp(60) / 2)
+                topMargin = (panelH - btnH)
             }
 
         panelShown = true
@@ -338,7 +338,7 @@ class BubbleService : Service() {
     private fun buildPanel(): FrameLayout {
         val panel =
             FrameLayout(this).apply {
-                setBackgroundColor(0x33000000.toInt())
+                setBackgroundColor(0x1A000000.toInt())
                 clipChildren = false
                 clipToPadding = false
             }
